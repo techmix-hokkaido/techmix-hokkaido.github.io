4b096076bb1b44d0f5a9ad795e427074951eeb3b
Kazuya NUMATA <numata@gmail.com>
2014-10-09 12:34:00 +0900

    Automated commit at 2014-10-09 03:34:00 UTC by middleman-deploy 0.2.4

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function(){
+	//googlemap
+	function initialize() {
+		var latlng = new google.maps.LatLng(43.063621,141.355319);
+		var myOptions = {
+			zoom: 15,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("googlemap"), myOptions);
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(43.063621,141.355319),
+			map: map,
+			title: ""
+		});
+	}
+	initialize();
+
+	//fixed glnav
+	var nav = $("#glnav");
+	var navTop = nav.offset().top;
+	$(window).scroll(function () {
+		if($(window).scrollTop() >= navTop) {
+			nav.addClass("fixed");
+			$("body").css("margin-top","40px");
+		} else {
+			nav.removeClass("fixed");
+			$("body").css("margin-top","0px");
+		}
+	});
+
+	//scroll
+	$('a[href^=#]').click(function() {
+		var speed = 400;
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position-100}, speed, 'swing');
+		return false;
+	});
+});
